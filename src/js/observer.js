3baae1cd35da0b2ccee0821bc85cd4792a1a2ca5
Petar <petar.purac@gmail.com>
2023-11-23 19:51:35 +0100

    Improve video logic and refactor code.

--- src/js/observer.js
+++ src/js/observer.js
@@ -1,8 +1,10 @@
 const observer = {
     textImageSection: document.querySelector('.js-text-image'),
+    videoPopup: document.querySelector('.js-video-popup'),
 
     init: function() {
         this.toggleClassIfVisible(document.querySelector('.js-diagram'), 'active');
+        this.trapFocus(this.videoPopup);
     },
 
      toggleClassIfVisible: function(el, className) {
@@ -14,12 +16,40 @@ const observer = {
             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
             rect.right <= (window.innerWidth || document.documentElement.clientWidth);
             if (isVisible) {
-              el.classList.add(className); // Add class if element is in viewport
+              el.classList.add(className);
             } else {
-              el.classList.remove(className); // Remove class if element is not in viewport
+              el.classList.remove(className);
             }
         })
+      },
+
+      trapFocus: function(element) {
+        var focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+        var firstFocusableEl = focusableEls[0];  
+        var lastFocusableEl = focusableEls[focusableEls.length - 1];
+        var KEYCODE_TAB = 9;
+      
+        element.addEventListener('keydown', function(e) {
+          var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+      
+          if (!isTabPressed) { 
+            return; 
+          }
+      
+          if ( e.shiftKey ) /* shift + tab */ {
+            if (document.activeElement === firstFocusableEl) {
+              lastFocusableEl.focus();
+                e.preventDefault();
+              }
+            } else /* tab */ {
+            if (document.activeElement === lastFocusableEl) {
+              firstFocusableEl.focus();
+                e.preventDefault();
+              }
+            }
+        });
       }
+
 
 
 }
--- src/js/video.js
+++ src/js/video.js
@@ -1,9 +1,11 @@
 import helpers from "./helpers";
 
 const video = {
+    playerDiv : document.getElementById("videoPlayer"),
     videoPopup: document.querySelector('.js-video-popup'),
     playButton : document.querySelector('.js-video-btn'),
     closePopupBtn: document.querySelector('.js-close-btn'),
+    
     isPlaying: false,
     
     init: function () {
@@ -12,33 +14,56 @@ const video = {
     },
 
     handleVideoPlay: function () {
+        const vimeoPlayer = new Vimeo.Player(this.playerDiv, {
+            id: 104926724,
+        });
         this.playButton?.addEventListener("click", () => {
             if(!this.isPlaying) {
                 this.isPlaying = true;
                 this.videoPopup.classList.add('video__popup--active');
                 helpers.disableScroll()
+                vimeoPlayer.play();
             }else {
                 this.videoPopup.classList.remove('video__popup--active')
                 this.isPlaying = false;
                 helpers.enableScroll()
+                vimeoPlayer.pause();
             }
         });
     },
 
     handleCloseVideoPopup: function() {
+      
+        const vimeoPlayer = new Vimeo.Player(this.playerDiv, {
+            id: 104926724,
+        });
+
         this.closePopupBtn.addEventListener('click', () => {
             this.videoPopup.classList.remove('video__popup--active')
             this.isPlaying = false;
             helpers.enableScroll()
+            vimeoPlayer.pause();
         })
-
+        
         document.addEventListener('keydown', (event) => {
             if (event.key === 'Escape') {
                 this.videoPopup.classList.remove('video__popup--active')
                 this.isPlaying = false;
+                vimeoPlayer.pause();
                 helpers.enableScroll()
             }
         });
+
+        this.videoPopup.addEventListener('click', (event) => {
+            if(this.isPlaying) {   
+                if((event.key !== 'Escape' || !event.target.classList.contains('js-close-btn'))) {
+                    this.videoPopup.classList.remove('video__popup--active')
+                    this.isPlaying = false;
+                    vimeoPlayer.pause();
+                    helpers.enableScroll()
+                }
+            }
+        })
     }
 }
 
